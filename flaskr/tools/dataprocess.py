d324f1cfd4e074b9e472bfe70919e927bba00471
aryanmhaskar <63806946+aryanmhaskar@users.noreply.github.com>
2022-05-22 17:18:25 -0400

    data processing but it works

--- flaskr/tools/dataprocess.py
+++ flaskr/tools/dataprocess.py
@@ -25,3 +25,16 @@ def stdev_distribution(id, data:list, num_samples:int, sample_size:int):
     x_bar = rs.std(axis=1)
     plt.hist(x_bar);
     plt.savefig(os.path.join(os.path.dirname(__file__), ("../static/" + str(id) + 'stdev.png')))
+
+def distribution(id, data:list):
+    rs = np.array(data)
+    plt.hist(data)
+    plt.savefig(os.path.join(os.path.dirname(__file__), ("../static/" + str(id) + 'dist.png')))
+
+def statistics_info(data:list):
+    rs = np.array(data)
+    return f"""The mean of the dataset is: {rs.mean()}
+    The standard deviation of the dataset is: {rs.std()} 
+    The max of the dataset is: {rs.max()}
+    The min of the dataset is: {rs.min()}
+    The range of the dataset is: {rs.max() - rs.min()} """
